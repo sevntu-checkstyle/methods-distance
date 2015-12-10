b0a010d5ef29e2b5f7b7274559ec71bfbaf01708
Zuy Alexey <zuy_alexey@mail.ru>
2015-12-10 19:31:36 +0300

    Added test support classes(copy-paste from checkstyle project)

---
+++ src/test/java/org/pirat9600q/graph/MethodCallDependencyCheckTest.java
@@ -0,0 +1,21 @@
+package org.pirat9600q.graph;
+
+import com.puppycrawl.tools.checkstyle.ConfigurationLoader;
+import com.puppycrawl.tools.checkstyle.DefaultConfiguration;
+import org.junit.Test;
+
+import javax.security.auth.login.Configuration;
+import java.io.IOException;
+
+public class MethodCallDependencyCheckTest extends BaseCheckTestSupport {
+
+    @Test
+    public void testThatWeCanWriteTests() throws Exception {
+        final DefaultConfiguration config = createCheckConfig(MethodCallDependencyCheck.class);
+        verify(config, getInputPath("Input1.java"));
+    }
+
+    private static String getInputPath(final String fileName) {
+        return MethodCallDependencyCheckTest.class.getResource(fileName).getPath();
+    }
+}
---
+++ src/test/resources/org/pirat9600q/graph/Input1.java
@@ -0,0 +1,11 @@
+package org.pirat9600q.graph;
+
+public class Input1 {
+    public void method() {
+
+    }
+
+    public void method2() {
+
+    }
+}
