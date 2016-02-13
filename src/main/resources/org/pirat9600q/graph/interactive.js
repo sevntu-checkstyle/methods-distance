9818961c00985e7116a04f0dae756a796027dca7
Zuy Alexey <zuy_alexey@mail.ru>
2016-02-13 23:08:14 +0300

    Improved highlighting

--- src/main/resources/org/pirat9600q/graph/interactive.js
+++ src/main/resources/org/pirat9600q/graph/interactive.js
@@ -1,25 +1,36 @@
 jQuery(function($) {
-    var table = $('#matrixTable');
-    var maxWidth = table.find('.horizontal-heading td')
-        .get()
-        .map(function(elem) {
-            return $(elem).width();
-        })
-        .sort(function(l, r) {
-            return parseInt(l) - parseInt(r);
-        })
-        .pop();
-    table.find('.horizontal-heading td').css('maxWidth', '30px');
-    table.find('tr.horizontal-heading>td').css('height', maxWidth);
+    function responsiveTable(tableSelector) {
+        var table = $(tableSelector);
+        var allHighlightableCells = table.find('td.highlightable');
+        var firstColumnCells = table.find('td:first-child');
+        table.find('td.cell').click(function(e) {
+            allHighlightableCells.removeClass('highlighted');
+            firstColumnCells.removeClass('dependency');
+            var td = $(e.target);
+            var clickedColumn = td.index();
+            var clickedRow = td.parent().index();
+            var cellsInRowOrColumn = `td.highlightable:nth-child(${clickedColumn + 1}),tr:nth-child(${clickedRow + 1})>td.highlightable`;
+            table.find(cellsInRowOrColumn).addClass('highlighted');
+            table.find(`tr:nth-child(${clickedColumn + 1})>td:first-child`).addClass('dependency');
+        });
+    }
 
-    table.find('td.cell').click(function(e) {
-        table.find('td.highlightable').removeClass('highlighted');
-        var td = e.target;
-        var column = $(td).index();
-        var row = $(td).parent().index();
-        console.log('Column: ' + column, 'Row: ' + row);
-        var selector = `td.highlightable:nth-child(${column + 1}),tr:nth-child(${row + 1})>td.highlightable`;
-        console.log('Selector: "' + selector + '"');
-        table.find(selector).addClass('highlighted');
-    });
+    function setUpTableDimensions(tableSelector) {
+        var table = $(tableSelector);
+        var maxWidth = table.find('.horizontal-heading td')
+            .get()
+            .map(function(elem) {
+                return $(elem).width();
+            })
+            .sort(function(l, r) {
+                return parseInt(l) - parseInt(r);
+            })
+            .pop();
+        table.find('.horizontal-heading td').css('maxWidth', '30px');
+        table.find('tr.horizontal-heading>td').css('height', maxWidth);
+    }
+
+    var tableSelector = '#matrixTable';
+    setUpTableDimensions(tableSelector);
+    responsiveTable(tableSelector);
 });
