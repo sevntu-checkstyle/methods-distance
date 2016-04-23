4cfd32ba532a66f2a5266550e903643a006224fc
Zuy Alexey <zuy.alexey@gmail.com>
2016-04-23 22:59:51 +0300

    Added empty servlet and jetty-maven-plugin

---
+++ src/main/java/org/pirat9600q/MainServlet.java
@@ -0,0 +1,14 @@
+package org.pirat9600q;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+public class MainServlet extends HttpServlet {
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException,
+            IOException { }
+}
